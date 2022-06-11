67fe372f67f20b8dc6313eaa0f967ffae2db0eca
liuxy0551 <liuxy0551@qq.com>
2022-06-11 10:40:58 +0800

    feat: yarn lock

---
+++ timed-task/func-refresh.js
@@ -0,0 +1,21 @@
+/**
+ * 函数刷新，避免冷启动
+ */
+const http = require('http')
+const { getNow } = require('./utils')
+
+exports.main_handler = async (event, context) => {
+    http.get('http://github-trending-api.liuxianyu.cn', (data) => {
+        let str = ''
+        data.on('data', (chunk) => {
+            str+=chunk;//监听数据响应，拼接数据片段
+        })
+        data.on('end', () => {
+            console.log(getNow(), str.toString())
+            return event
+        })
+    })
+}
+
+// 0 0/2 * * * ?
+// 或 每两分钟

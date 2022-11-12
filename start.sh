0849a03f9977d76724ee6adbdb0bdb91874bdabb
liuyi <>
2022-11-12 20:09:48 +0800

    feat: start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 # git clone https://github.com/liuxy0551/github-trending-api
-
-git add src/config/
+git add pm2 src/config/app.config.js
 git pull origin master
 yarn
 pm2 restart ./pm2/config.json --env production
